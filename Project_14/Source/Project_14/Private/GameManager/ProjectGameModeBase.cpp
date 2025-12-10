15ce44a98c3474ec26e27bc28b3606caf872c0ad
WrongCat7 <wrongcat7@gmail.com>
2025-12-10 11:56:08 +0900

    Revert "Merge pull request #9 from NbcampUnreal/Gimmick/Level/SIC"
    This reverts commit cd1361623b46d80d8c4f7c19995f350ddeda0d12, reversing
    changes made to 473b0881243d2f50cc7d4a2ab73c7d3bb001ddc7.

--- Project_14/Source/Project_14/Private/GameManager/ProjectGameModeBase.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameManager/ProjectGameModeBase.h"
-#include "Player/PlayerCharacter.h"
-#include "Player/PlayerCtr.h"
-
-AProjectGameModeBase::AProjectGameModeBase()
-{
-	DefaultPawnClass = APlayerCharacter::StaticClass();
-	PlayerControllerClass = APlayerCtr::StaticClass();
-}
--- Project_14/Source/Project_14/Project_14.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Project_14.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_14, "Project_14" );
- 
