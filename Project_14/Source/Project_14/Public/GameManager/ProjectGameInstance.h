aa84b085460bda26aba76d6e81b7477656e26d80
chomuhyun <156655809+chomuhyun@users.noreply.github.com>
2025-12-15 19:55:26 +0900

    Add / Fix
    레플리케이션중

---
+++ Project_14/Source/Project_14/Public/GameManager/ProjectGameInstance.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Player/PlayerCharacter.h"
+#include "ProjectGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_14_API UProjectGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintReadWrite, Category = "Character Select")
+	ECharacterType SelectedCharacterType;
+};
