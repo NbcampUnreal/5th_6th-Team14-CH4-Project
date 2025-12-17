c9014260e1ce21ea816035e9b1ff0c9b84612639
BK <quannie70@gmail.com>
2025-12-17 20:01:55 +0900

    12/17 fix
    수정

--- Project_14/Source/Project_14/Private/Gimmick/BK/ObstacleBall.cpp
+++ Project_14/Source/Project_14/Private/Gimmick/BK/ObstacleBall.cpp
@@ -1,6 +1,7 @@
 #include "Gimmick/BK/ObstacleBall.h"
 #include "Components/StaticMeshComponent.h"
 #include "TimerManager.h"
+#include "Engine/World.h"
 
 AObstacleBall::AObstacleBall()
 {
@@ -14,6 +15,9 @@ AObstacleBall::AObstacleBall()
 
     Mesh->SetMobility(EComponentMobility::Movable);
     Mesh->SetCollisionProfileName(TEXT("PhysicsActor"));
+
+    Mesh->SetNotifyRigidBodyCollision(true);
+    Mesh->OnComponentHit.AddDynamic(this, &AObstacleBall::OnHit);
 }
 
 void AObstacleBall::BeginPlay()
@@ -36,5 +40,39 @@ void AObstacleBall::BeginPlay()
 void AObstacleBall::EnablePhysics()
 {
     Mesh->SetSimulatePhysics(true);
+
+    Mesh->SetMassOverrideInKg(NAME_None, NormalMass, true);
     Mesh->WakeAllRigidBodies();
+}
+
+void AObstacleBall::OnHit(
+    UPrimitiveComponent* HitComp,
+    AActor* OtherActor,
+    UPrimitiveComponent* OtherComp,
+    FVector NormalImpulse,
+    const FHitResult& Hit
+)
+{
+    if (!HasAuthority())
+        return;
+
+    if (NormalImpulse.Size() > PushImpulseThreshold)
+    {
+        Mesh->SetMassOverrideInKg(NAME_None, PushableMass, true);
+
+        GetWorldTimerManager().ClearTimer(ResetMassTimer);
+        GetWorldTimerManager().SetTimer(
+            ResetMassTimer,
+            [this]()
+            {
+                if (Mesh)
+                {
+                    Mesh->SetMassOverrideInKg(NAME_None, NormalMass, true);
+                }
+            },
+            0.4f,
+            false
+        );
+    }
+
 }
