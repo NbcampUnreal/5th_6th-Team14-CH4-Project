284d683c48b6d9cb530065c023513a1928b03d04
Hoseok-hth <heart12500@gmail.com>
2025-12-19 20:27:56 +0900

    chatting system

--- Project_14/Source/Project_14/Private/GameManager/ProjectGameInstance.cpp
+++ Project_14/Source/Project_14/Private/GameManager/ProjectGameInstance.cpp
@@ -1,2 +1,12 @@
 #include "GameManager/ProjectGameInstance.h"
+#include "SocketIOClientComponent.h"
+
+void UProjectGameInstance::Init()
+{
+	Super::Init();
+
+	
+}
+
+
 
---
+++ Project_14/Source/Project_14/Public/Chatting/ChatInput.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ChatInput.generated.h"
+
+class UScrollBox;
+class UEditableTextBox;
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_14_API UChatInput : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UEditableTextBox> EditableTextBox_ChatInput;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UScrollBox> ChatScrollBox;
+
+	virtual void NativeConstruct() override;
+	virtual void NativeOnInitialized() override;
+
+	void AddMessage(FString Sender, FString Msg);
+
+protected:
+	UFUNCTION()
+	void OnChatInputTextCommitted(const FText& Text, ETextCommit::Type CommitMethod);
+	
+};
